72ed735fa24415c50faac66a240d7b0ffb578b54
Ivan Besere <apapomsV2@gmail.com>
2026-02-04 17:58:56 +0800

    Problem 3 solved

---
+++ problem3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $n = 101;
+        if ($n % 2 === 0) {
+            echo "EVEN";
+        }else{
+            echo "ODD";
+        }
+    ?>
+</body>
+</html>
