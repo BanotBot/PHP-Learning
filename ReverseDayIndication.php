fda48b124144569a53f54898a57a415542923542
Ivan Besere <apapomsV2@gmail.com>
2026-05-24 00:04:11 +0800

    ReverseDayIndication problem has the best solution update

--- ReverseDayIndication.php
+++ ReverseDayIndication.php
@@ -1,45 +1,25 @@
 <?php
 
-$data = [
-    'MON'   => 1, 
-    'TUE'   => 2,
-    'WED'   => 4,
-    'THU'   => 8,
-    'FRI'   => 16,
-    'SAT'   => 32,
-    'SUN'   => 64
-];
 
-function reverseDay($data, $value) {
-    arsort($data);
-    var_dump($data);
-
-    $current_total = 0;
-    $output = [];
+function reverseDay($value) {
     
-    foreach ($data as $key => $val) {
-        // check if val > 42
-        if ($val > $value) {
-            continue;
+    $data = [
+        'MON'   => 1, 
+        'TUE'   => 2,
+        'WED'   => 4,
+        'THU'   => 8,
+        'FRI'   => 16,
+        'SAT'   => 32,
+        'SUN'   => 64
+    ];
+
+    foreach ($data as $key => $bit) {
+        if (($value & $bit) !== 0) {
+            $output[] = $key;
         }
-
-        // echo $val;
-        $current_total += $val; // 0 + 32 = 32, 32 + 16 = 48,  32 + 8 = 40, 40 + 4 = 44, 40 + 2 = 42
-
-        if ($current_total > $value) {
-            $current_total -= $val; // 48 - 16 = 32, 40 - 4 = 42
-            continue;
-        }  
-
-        $output[] = $key;
     }
 
     return $output;
 }
 
-$result = reverseDay($data, 21);
-krsort($result);
-echo '<br>';
-foreach ($result as $key => $value) {
-    echo $value . ((array_key_last($result) === $key) ? '' : ',');
-}
+echo implode(',', reverseDay(21));
